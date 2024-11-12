fd2c9efa98aba9c5bc80b334d02999d1870d3ec5
Jesus-bit <jesus.gomez.7089@gmail.com>
2024-11-11 19:51:37 -0600

    fix warning useEffect

--- src/components/weather/WeatherCard.tsx
+++ src/components/weather/WeatherCard.tsx
@@ -77,6 +77,7 @@ export default function WeatherCard({ onDistrictChange }: WeatherCardProps) {
   // Cargar datos iniciales
   useEffect(() => {
     handlePredictWeather()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
